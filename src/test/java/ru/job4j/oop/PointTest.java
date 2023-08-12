e5fd222e9fb26cbfffaaae2621cd8ef5b9fef49c
Nikita Gromov <nikita.gromow.2000@gmail.com>
2023-08-12 09:54:01 +0300

    2.0. Рефакторинг - Расстояние между точками. [#122587 #435462]

--- src/test/java/ru/job4j/oop/PointTest.java
+++ src/test/java/ru/job4j/oop/PointTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 
 public class PointTest {
     @Test
-    public void when00to20then2() {
+    public void when00to02then2() {
         double expected = 2;
         Point first = new Point(0, 0);
         Point second = new Point(0, 2);
@@ -14,7 +14,7 @@ public class PointTest {
     }
 
     @Test
-    public void when20to00then2() {
+    public void when2minus1to04then5dot38() {
         double expected = 5.38;
         Point first = new Point(2, -1);
         Point second = new Point(0, 4);
@@ -23,7 +23,7 @@ public class PointTest {
     }
 
     @Test
-    public void when02to00then2() {
+    public void when84to91then3dot16() {
         double expected = 3.16;
         Point first = new Point(8, 4);
         Point second = new Point(9, 1);
@@ -32,7 +32,7 @@ public class PointTest {
     }
 
     @Test
-    public void when00to02then2() {
+    public void whenMinus52to02then3minus4() {
         double expected = 10;
         Point first = new Point(-5, 2);
         Point second = new Point(3, -4);
